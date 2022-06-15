c8cd7ae7199100664d57c473988b5608da929077
João Pster <joaopsterdev@gmail.com>
2022-06-15 19:28:58 -0300

    Resolvido o problema do React Router Dom em Páginas GitHub

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {
   BrowserRouter,
+  HashRouter,
   Route,
   Switch,
 } from 'react-router-dom';
@@ -18,15 +19,17 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/search" component={ Search } />
-          <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/favorites" component={ Favorites } />
-          <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/" component={ Login } />
-          <Route component={ NotFound } />
-        </Switch>
+        <HashRouter basename="/">
+          <Switch>
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/" component={ Login } />
+            <Route component={ NotFound } />
+          </Switch>
+        </HashRouter>
       </BrowserRouter>
     );
   }
